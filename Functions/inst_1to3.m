0018dc7ef7e6c29337d5b3dc90cfb0bb4d6226af
Felix Weingartner <u1138255@utah.edu>
2018-03-14 20:55:36 -0600

    updated files

--- Functions/inst_1to3.m
+++ Functions/inst_1to3.m
@@ -7,14 +7,14 @@
 
 % Define A
 A = [ 4, 1, -2;  4, 4, -3;  8, 4, 0; ];
-
+delim = ' ';
 % Compute L and U
 [ L, U ] = lu(A);
 
 % Print L and U
 addpath('Outputs');
-dlmwrite('Outputs/LU_Decomp_Inst1_1.txt', L, 'delimiter', ' ');
-dlmwrite('Outputs/LU_Decomp_Inst1_1.txt', U, '-append', 'delimiter', ' ');
+dlmwrite('Outputs/LU_Decomp_Inst1_1.txt', L, 'delimiter', delim);
+dlmwrite('Outputs/LU_Decomp_Inst1_1.txt', U', '-append', 'delimiter', delim);
 
 % ...
 % 2) Using this decomposition determine the solution using Ax
@@ -25,7 +25,7 @@ Ax = [ 0;  3;  16; ];
 y = L\Ax;
 % Ux = y find our final solution x
 x = U\y;
-dlmwrite('Outputs/LU_Decomp_Inst2.txt', x, 'delimiter', ' ');
+dlmwrite('Outputs/LU_Decomp_Inst2.txt', x, 'delimiter', delim);
 
 % ...
 % 3) Compute the LU decompositions of B and C
@@ -35,8 +35,8 @@ B = [ 4, 1, -2;  4, 4, -3;  8, 4, 2; ];
 C = [ 2, 1, -2;  4, 4, -3;  8, 4, 4; ];
 
 [ L, U ] = lu(B);
-dlmwrite('Outputs/LU_Decomp_Inst3.txt', L, 'delimiter', ' ');
-dlmwrite('Outputs/LU_Decomp_Inst3.txt', U, '-append', 'delimiter', ' ');
+dlmwrite('Outputs/LU_Decomp_Inst3.txt', L, 'delimiter', delim);
+dlmwrite('Outputs/LU_Decomp_Inst3.txt', U, '-append', 'delimiter', delim);
 [ L, U ] = lu(C);
-dlmwrite('Outputs/LU_Decomp_Inst3.txt', L, '-append', 'delimiter', ' ');
-dlmwrite('Outputs/LU_Decomp_Inst3.txt', U, '-append', 'delimiter', ' ');
+dlmwrite('Outputs/LU_Decomp_Inst3.txt', L, '-append', 'delimiter', delim);
+dlmwrite('Outputs/LU_Decomp_Inst3.txt', U, '-append', 'delimiter', delim);
--- Functions/inst_4.m
+++ Functions/inst_4.m
@@ -1,136 +1,119 @@
-% Solving ill-conditioned matrix problem using iterative refinement
+% Solving ill-conditioned matrix problem ujng iterative refinement
 % Written by Felix Weingartner for 3200 Hw3
 
-NValues = [ 21, 41, 81, 161 ];
+clear;
+
+
+nValues = [ 21, 41, 81, 161 ];
+nValuesSize = numel(nValues);
+nCondTime = 0.0;
+
 aValues = [ 1.0, 1.0e-1, 1.0e-3, 1.0e-5, 1.0e-7, 1.0e-9, 1.0e-11, 1.0e-13, 1.0e-15 ];
+aValuesSize = numel(aValues);
+
+
 dx = 1;
 H1 = 8;
 Hr = 4;
 
+addpath('Outputs');
 file1 = fopen('Outputs/Inst4.txt', 'wt');
-format = "N:%d\ta:%d\tCond:%0.5f\tSol:%s\n";
+format1 = "\nN:%d\ta:%0.2e\n";
+format2 = "Cond:%0.5e\tSol:%s\n";
 
 
-for n = NValues
-   for a = aValues
-       
-      % Generate matrix G
-      
-      c = 1/dx^2;
-      G = zeros(n, n);
-      % glob vars
-      i = 0;
-      value = 0;
-      
-      % Top rows
-      
-      for i = 1:n/2
-          
-          % Column
-          
-          for j = 1:n
-              
-              % -2 and 1
-              
-              if (mod(i, 2) == 1)
-                  
-                % Odd columns are -2 and even columns are 1
-                
-                if (mod(j, 2) == 1)
-                    value = -2;
-                else
-                    value = 1;
-                end
-              else
-                  
-                % Odd columns are 1 and even columns are -2
-                
-                if (mod(j, 2) == 1)
+
+for nIndex = 1:numel(nValues)
+    n = nValues(nIndex);
+    for aIndex = 1:numel(aValues)
+        a = aValues(aIndex);
+        % Generate matrix G
+        
+        c = 1/dx^2;
+        G = zeros(n, n);
+        % glob vars
+        value = 0;
+        
+        % First row
+        
+        i = 1;
+        G(1,1) = -2;
+        g(1,2) = 1;
+        
+        % Top rows
+        
+        for i = 2:int32(n/2)
+            % Get start index for 1 -2 1 seq
+            
+            j_start = i - 1;
+            
+            for j = j_start:j_start+3
+                if (j == j_start || j == j_start+2)
                     value = 1;
                 else
                     value = -2;
                 end
-              end
-              
-              G(i, j) = value;
-              
-          end
-      end
-      
-      % Middle row
-      if (mod(n/2, 2) == 1)
-          
-          i = i + 1;
-          
-          for j = 1:n  
-            % 3-1 columns are 1, 3-2 columns are -(1+a), and 3-3
-            % columns are a
-
-            if (mod(j, 3) == 1)
+                G(i, j) = value;
+            end
+        end
+        
+        % Middle row
+        
+        i = i + 1;
+        % Get start index for the 1 -(1+a) a seq
+        j_start = int32(n/2);
+        for j = j_start:j_start+3
+            if (j == j_start)
                 value = 1;
-            elseif (mod(j, 3) == 2)
+            elseif (j == j_start + 1)
                 value = (-1)*(1+a);
             else
                 value = a;
             end
             
             G(i, j) = value;
-          end
-      end
-      
-      % Bottom rows
-      for i = i+1 : n
-          
-        % Column
-          
-          for j = 1:n
-              
-              % a, -2a
-              
-              if (mod(i, 2) == 1)
-                  
-                % Odd columns are a and even columns are -2a
-                
-                if (mod(j, 2) == 1)
+        end
+        
+        % Bottom rows
+        for i = i+1 : n
+            
+            % Get start index for the a -2a a seq
+            j_start = i - 1;
+            for j = j_start:j_start+3
+                if (j == j_start || j == j_start+2)
                     value = a;
-                else
-                    value = -2*a;
+                else (j == j_start + 1)
+                    value = (-1)*(1+a);
                 end
-              else
-                  
-                % Odd columns are -2a and even columns are a
                 
-                if (mod(j, 2) == 1)
-                    value = -2*a;
-                else
-                    value = a;
-                end
-              end
-              
-              G(i, j) = value;
-              
-          end
-      end
-      
-      % Generate result matrix Gh with dimensions nx1
-      Gh = zeros(n,1);
-      Gh(1,1) = -H1;
-      Gh(n,1) = -a*Hr;
-      
-      % We have cG(h) = Gh with h solutions unknown
-      G = c.*G;
-      % Generate h
-      H = linsolve(G, Gh);
-      % Generate h as a string
-      hStr = mat2str(H);
-      % Generage Condition number
-      mCond = cond(G);
-      
-      % Print values
-      fprintf(file1, format, [n, a, mCond, hStr]);
-      
-
-   end
+                G(i, j) = value;
+            end
+        end
+        
+        % Generate result matrix Gh with dimenjons nx1
+        Gh = zeros(n,1);
+        Gh(1,1) = -H1;
+        Gh(n,1) = -a*Hr;
+        
+        % We have cG(h) = Gh with h solutions unknown
+        G = c*G;
+        % Generate h
+        
+        H = linsolve(G, Gh);
+        % Generate h as a string
+        hStr = mat2str(H);
+        % Generage Condition number
+        tic;
+        nCond = cond(G);
+        nCondTime = toc;
+        disp(nCondTime);
+        fprintf(file1, format1, [n, a]);
+        
+        % Print values
+        fprintf(file1, format2, [nCond, hStr]);
+        fmt = [repmat('%0.4e ', 1, size(H, 2)-1), '%0.4e\n'];
+        fprintf(file1, fmt, H.');
+        
+    end
 end
-
-
+fclose(file1);
